39fd877aa46b30640848723c3249f0b245492ec2
moysescosta <mecs.cwb@gmail.com>
2025-01-03 13:39:06 -0300

    #

--- tipos-variaveis/src/Metodos/MyClass.java
+++ tipos-variaveis/src/Metodos/MyClass.java
@@ -1,10 +1,15 @@
 package Metodos;
-//EXEMPLOS DAS AULAS
+
+public class MyClass {
+    public static void main(String[] args) {
+        
+    }
+
+    //EXEMPLOS DAS AULAS
 
 int num1;
 int num2;
 
-public class MyClass {
     public double somar(int num1, int num2){
         return(num1 + num2);
     }
