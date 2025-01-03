6a79f3d21f2875ac431000a0e9d00a022b7a78ee
moysescosta <mecs.cwb@gmail.com>
2025-01-03 14:34:52 -0300

    #

---
+++ sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,30 @@
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
+    public void aumentarCanal(){
+        canal++;
+    }
+    public void diminuirCanal(){
+        canal--;
+    }
+    public void aumetarVolume(){
+        volume++; //<- igual à volume = volume + 1;
+        System.out.println("Aumentando o volume para: " + volume);
+    }
+    public void diminuirVolume(){
+        volume--; //<- igual à volume = volume - 1;
+        System.out.println("Diminuindo o volume para: " + volume);
+    }
+
+    public void ligar(){
+        ligada = true;
+    }
+
+    public void desligar(){
+        ligada = false;
+    }
+}
