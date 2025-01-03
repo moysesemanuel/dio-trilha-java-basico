561dfc54fe0df62b978f5956c3a137d8acfb5385
moysescosta <mecs.cwb@gmail.com>
2025-01-03 14:04:24 -0300

    #

--- tipos-variaveis/src/Metodos/MyClass.java
+++ tipos-variaveis/src/Metodos/MyClass.java
@@ -19,4 +19,37 @@ public class MyClass {
         return num1 + num2;
     }
 
+    public void imprimir(String texto){
+        /*LOGICA - FINALIDADE DO MÉTODO
+          AQUI NÃO PRECISA DO RETURN, POIS NÃO SERÁ RETORNADO NENHUM VALOR
+        */
+    }
+    //throw Exception : indica que o método ao ser utilizado poderá gerar uma exceção.
+
+    public double dividir(int dividendo, int divisor) throws Exception{
+            return divisor;}
+
+    //este método não pode ser visto por outras classes no programa
+    @SuppressWarnings("unused")
+    private void metodoPrivado(){}
+
+    //alguns equívocos estruturais
+    public void validar(){
+        //este método deveria retornar algum valor
+        //no caso boolean (true ou flase)
+    }
+        
+
+    public void calcularEnviar(){
+        //um método deve representar uma única resposta
+    }
+        
+    public void gravarCliente(String nome, String cpf, Integer idade){
+        //este método te a finalidade de gravar informações de um cliente, por que não criar um objeto cliente e passar como parâmetro?
+        //veja abaixo
+    }
+    
+    public <Cliente> void gravarCliente(Cliente cliente){}
+    //ou
+    public <Cliente> void gravar(Cliente cliente){}
 }
