92831efe28c6170866eabe4a0f0b4755afdebf36
moysescosta <mecs.cwb@gmail.com>
2025-01-07 13:04:06 -0300

    Condicionais

--- estrutura_condicional/EstruturaCondicional/src/ResultadoEscolar.java
+++ estrutura_condicional/EstruturaCondicional/src/ResultadoEscolar.java
@@ -1,15 +1,33 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 10;
 
-        if(nota >= 7)
+        //ENCADEADO
+        int nota1 = 10;
+
+        if(nota1 >= 7)
             System.out.println("Aprovado");
 
-            else if(nota >= 5 && nota < 7)
+            else if(nota1 >= 5 && nota1 < 7)
             System.out.println("Prova de Recuperação");
         
         else
             System.out.println("Reprovado");
+
+        //TERNÁRIO IF/ELSE
+        int nota2 = 5;
+                // SE        nota\/       \/   SENÃO   \/
+        String resultado1 = nota2 >=7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado1);
+
+        //IF/ELSE-IF/ELSE
+        int nota3 = 6;
+        String resultado2 = nota3 >=7 ? "Aprovado" : nota3 >=5 && nota3 <7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado2);
+
+
+
+
+
     }
     
 }
