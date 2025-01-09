2700e7b60435831661d93296d4549afa79fdfbd8
moysescosta <mecs.cwb@gmail.com>
2025-01-09 10:33:23 -0300

    For em Arrays

--- estrutura_condicional/EstruturaCondicional/src/ExemploFor.java
+++ estrutura_condicional/EstruturaCondicional/src/ExemploFor.java
@@ -3,6 +3,12 @@ public class ExemploFor {
         for(int carneiro = 1; carneiro <= 20; carneiro ++){
             System.out.println("Contando carneiros " + carneiro);
         }
+        //em arrays o indice de elementos inicia em zero
+        String alunos[] = {"FELIPE", "JONAS", "JULIA", "MARCOS"};
+
+        for (int x=0; x<alunos.length; x++) {
+            System.out.println("O aluno no índice x=" + x + " é " + alunos[x]);
+        }
     }
     
 }
