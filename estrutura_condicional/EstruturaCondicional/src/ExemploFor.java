510e3c486e24c2b8ae18c5b09620e04f931f73f0
moysescosta <mecs.cwb@gmail.com>
2025-01-09 10:06:42 -0300

    Exemplo for em Java

---
+++ estrutura_condicional/EstruturaCondicional/src/ExemploFor.java
@@ -0,0 +1,8 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        for(int carneiro = 1; carneiro <= 20; carneiro ++){
+            System.out.println("Contando carneiros " + carneiro);
+        }
+    }
+    
+}
